764daec71c8376aae08ba2e4d60e305185e19c4e
mauroradino <mauroradino2003@gmail.com>
2025-10-22 02:19:58 -0300

    second video

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -10,7 +10,15 @@ function Videos(){
   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
   allowfullscreen>
 </iframe>
-
+<iframe 
+  width="560" 
+  height="315" 
+  src="https://www.youtube.com/embed/5M4gKJ6Ipvk"
+  title="YouTube video player" 
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+  allowfullscreen>
+</iframe>
         </div>
     )
 }
